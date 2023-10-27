48485ae7e7d5daeec28ca112d06ec7d67d22a678
SengokuGarp <garpirungu@gmail.com>
2023-10-27 15:22:30 +0300

    Completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links() {
+  return (
+    <div>
+      <h3>Links</h3>
+      {/* Add your GitHub and LinkedIn links here */}
+      <a href="https://github.com/your-username">GitHub</a>
+      <a href="https://www.linkedin.com/in/your-profile">LinkedIn</a>
+    </div>
+  );
+}
+
+export default Links;
